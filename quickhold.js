c2c4d64bc203a7a13d3bb604c5174a3b66002176
marinus <marinus@laurabo.dk>
2019-05-07 14:44:08 +0200

    made login in quickhold

--- quickhold.js
+++ quickhold.js
@@ -1,42 +1,65 @@
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    document.getElementById("user_div").style.dispaly;
+    document.getElementById("login_div").style.dispaly;
+    document.getElementById("createlogin_div").style.dispaly;
 
+    $('#createlogin_div').hide();
+    $('#user_div').show();
+    $('#login_div').hide();
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-        document.getElementById("user_div").style.dispaly.show();
-        document.getElementById("login_div").style.dispaly.hide();
+  } else {
+    // No user is signed in.
+    document.getElementById("user_div").style.dispaly;
+    document.getElementById("login_div").style.dispaly;
+    document.getElementById("createlogin_div").style.dispaly;
 
-    } else {
-      // No user is signed in.
-      document.getElementById("user_div").style.dispaly.hide();
-      document.getElementById("login_div").style.dispaly.show();
+    $('#createlogin_div').hide();
+    $('#user_div').hide();
+    $('#login_div').show();
+  }
+});
 
-    }
+function login() {
+
+  var userEmail = document.getElementById("email_field").value;
+  var userpassword = document.getElementById("password_field").value;
+
+  console.log(userEmail + " " + userpassword);
+
+  //firebase.auth().createUserWithEmailAndPassword(userEmail, userpassword)
+  firebase.auth().signInWithEmailAndPassword(userEmail, userpassword).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error: " + errorMessage);
+    // ...
   });
 
-function login(){
-
-    var userEmail = document.getElementById("email_field").value;
-    var userpassword = document.getElementById("password_field").value;
-
-    console.log(userEmail + " " + userpassword);
-
-    firebase.auth().createUserWithEmailAndPassword(userEmail, userpassword).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-
-        window.alert("Error: " + errorMessage);
-        // ...
-      });
-   
 }
 
-function logout(){
+function create() {
 
-    firebase.auth().signOut().then(function() {
-        // Sign-out successful.
-      }).catch(function(error) {
-        // An error happened.
-      });
+  var userEmail = document.getElementById("email_field2").value;
+  var userpassword = document.getElementById("password_field2").value;
+
+  console.log(userEmail + " " + userpassword);
+
+  //firebase.auth().createUserWithEmailAndPassword(userEmail, userpassword)
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userpassword).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error: " + errorMessage);
+    // ...
+  });
+
+}
+
+function logout() {
+
+  firebase.auth().signOut()
 }
